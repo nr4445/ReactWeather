986511391b26406599f6d20f078d3e9874a8a76c
Naresh Pallothu <nareshpallothu@gmail.com>
2017-03-16 23:40:22 -0400

    message to be added

--- example-arrow-functions.js
+++ example-arrow-functions.js
@@ -22,26 +22,36 @@ var names = ['John','Matt','Ben'];
 //     });
 //   }
 // };
+
+var person = {
+  name: 'Naresh',
+  greet: function (){
+    names.forEach(function(name){
+      console.log(this === global); // Will print true with ES5 function
+      console.log(this+ 'says hai to '+ name)
+    });
+  }
+};
+
+person.greet();
+
+
+// function add(a, b){
+//   return a + b;
+// }
 //
-// person.greet();
-
-
-function add(a, b){
-  return a + b;
-}
-
-// console.log(add(1,3));
-// console.log(add(9, 0));
-
-//add statement
-var addStatement = (a, b) => {
-  return a + b;
-}
-
-console.log(addStatement(4, 7));
-
-//add expression
-
-var addExpression = (a,b) => a + b;
-console.log(addExpression(2,3));
-console.log(addExpression(2, -3));
+// // console.log(add(1,3));
+// // console.log(add(9, 0));
+//
+// //add statement
+// var addStatement = (a, b) => {
+//   return a + b;
+// }
+//
+// console.log(addStatement(4, 7));
+//
+// //add expression
+//
+// var addExpression = (a,b) => a + b;
+// console.log(addExpression(2,3));
+// console.log(addExpression(2, -3));
